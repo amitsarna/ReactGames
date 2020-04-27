e7e4d1df490084c16dc1fc5241e219f54e222ad6
amitsarna <amit.sarna.584@gmail.com>
2020-04-27 22:32:12 +0100

    Updates

---
+++ precache-manifest.5f574f80efdc64e4c9fded60c46100e6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251d064e271de453aa142b0134bab5b8",
+    "url": "/PicturePuzzle/index.html"
+  },
+  {
+    "revision": "7737bd3ae479b7890e6c",
+    "url": "/PicturePuzzle/static/css/main.a85f6368.chunk.css"
+  },
+  {
+    "revision": "050de58d6f63856cc693",
+    "url": "/PicturePuzzle/static/js/2.b122c175.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PicturePuzzle/static/js/2.b122c175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7737bd3ae479b7890e6c",
+    "url": "/PicturePuzzle/static/js/main.55537b14.chunk.js"
+  },
+  {
+    "revision": "73ca73fb6ae355ff3214",
+    "url": "/PicturePuzzle/static/js/runtime-main.c0465d73.js"
+  },
+  {
+    "revision": "d95e06db59a70750d585f6d4d5df8cf2",
+    "url": "/PicturePuzzle/static/media/earth.d95e06db.jpg"
+  },
+  {
+    "revision": "17e986cc8d63d4c4837548d9cc828ae6",
+    "url": "/PicturePuzzle/static/media/jupiter.17e986cc.jpg"
+  },
+  {
+    "revision": "36d76d820cc6e169a13919d63d63d0af",
+    "url": "/PicturePuzzle/static/media/mars.36d76d82.jpg"
+  },
+  {
+    "revision": "010ddae6eaa456110692ad5bc539d84a",
+    "url": "/PicturePuzzle/static/media/mercury.010ddae6.jpg"
+  },
+  {
+    "revision": "885d6c6463f864436ad448c8b074596b",
+    "url": "/PicturePuzzle/static/media/neptune.885d6c64.jpg"
+  },
+  {
+    "revision": "fe48b24996f1bfef973f4bafc638b70d",
+    "url": "/PicturePuzzle/static/media/saturn.fe48b249.jpg"
+  },
+  {
+    "revision": "678acd6faeab753aab3db2f26158cc7b",
+    "url": "/PicturePuzzle/static/media/uranus.678acd6f.jpg"
+  },
+  {
+    "revision": "9cf4b13f906df7886aa85a512e995546",
+    "url": "/PicturePuzzle/static/media/venus.9cf4b13f.jpg"
+  }
+]);
